c6f909bbce012323b36c5886422026842aa0fc32
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-08-06 12:07:00 +0200

    spring boot initialization

---
+++ src/main/java/eu/arcangelovicedomini/tombola/webapp/WebappApplication.java
@@ -0,0 +1,12 @@
+package eu.arcangelovicedomini.tombola.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+}
